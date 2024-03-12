ebf216a86fd1862c19d2f30ca77a54d56c40ca77
Lautaro <lautaroef@gmail.com>
2024-03-12 19:33:37 -0300

    Clean up

--- src/index.ts
+++ src/index.ts
@@ -9,33 +9,6 @@ import loanApplicationRoutes from './routes/loanApplicationRoutes';
 const app = express();
 const port = process.env.PORT || 3000;
 
-/*
-Core Features:
-- POST /applications
-- GET /applications/{id}
-- GET /applications
-
-Database Schema and ORM:
-- Tables for LoanApplications, Users, and Roles
-- Integrate Sequelize with models
-
-Authentication and Authorization:
-- POST /auth/register
-- POST /auth/login: Allow users to login and manage JWT token
-- Middleware for JWT token verification - middleware/verifyToken.ts
-- Middleware for role-based access control for distinguishing between Admin and Applicant users - middleware/roleCheck.ts
-
-Security measures:
-- Used DB_NAME, DB_PASSWORD and JWT_SECRET from environment variables
-- Implement password hashing and salting using `bcrypt`
-- Implement input validation using `express-validator`
-- Implement rate limiting for login attempts 
-
-Docker integration
-- Create a Dockerfile for the application
-- Create a `docker-compose.yml` file to run the app and postgres database together
-*/
-
 const loginLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 5, // Limit each IP to 5 login requests per windowMs
@@ -65,3 +38,29 @@ app.listen(port, async () => {
 });
 
 // TODO: move validation to a separate file
+/*
+Core Features:
+- POST /applications
+- GET /applications/{id}
+- GET /applications
+
+Database Schema and ORM:
+- Tables for LoanApplications, Users, and Roles
+- Integrate Sequelize with models
+
+Authentication and Authorization:
+- POST /auth/register
+- POST /auth/login: Allow users to login and manage JWT token
+- Middleware for JWT token verification - middleware/verifyToken.ts
+- Middleware for role-based access control for distinguishing between Admin and Applicant users - middleware/roleCheck.ts
+
+Security measures:
+- Used DB_NAME, DB_PASSWORD and JWT_SECRET from environment variables
+- Implement password hashing and salting using `bcrypt`
+- Implement input validation using `express-validator`
+- Implement rate limiting for login attempts 
+
+Docker integration
+- Create a Dockerfile for the application
+- Create a `docker-compose.yml` file to run the app and postgres database together
+*/
